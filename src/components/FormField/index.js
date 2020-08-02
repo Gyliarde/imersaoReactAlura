b3cb1b1ecdde32ee26b038d5cc974452e7a6f60b
Gyliarde Farias Araujo <gyliarde.farias@infopacx.com.br>
2020-08-02 11:47:41 -0300

    ADD: Eslint config and PropTypes

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,22 +1,33 @@
-import React from "react"
+import React from 'react';
+import PropTypes from 'prop-types';
 
-
-function FormField( {label,type,name, value, onChange} ) {
-    return (
-        <div>
-        <label>
-          {label}
-                  <input 
-                  type={type}
-                  name={name}
-                  value={value}
-                  onChange={onChange}
-            ></input>
-        </label>
-      </div>
-    )
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  return (
+    <div>
+      <label>
+        {label}
+        <input
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
 }
 
-export default FormField;
 
-   
+FormField.propTypes = {
+  label, 
+  type, 
+  name, 
+  value, 
+  onChange,
+}
+
+// Aula 4 17:00 MIn
+
+export default FormField;
