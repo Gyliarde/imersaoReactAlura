a5adb9f20bbd8db157013f97f7433622abe31076
Gyliarde Araujo <gyliardearaujo@Air-de-Gyliarde.meuintelbras.local>
2020-08-04 21:18:26 -0300

    FIX: link heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -36,7 +36,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if(window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias'; 
+      const URL = 'https://devflixdepalura.herokuapp.com/categorias'; 
       fetch(URL)
        .then(async (respostaDoServer) =>{
         if(respostaDoServer.ok) {
